ca31db2d3142813618ed867259d0ba03ab1c9625
denisecur <denise.geiger@icloud.com>
2024-05-28 21:31:38 +0200

    Home Text angepasst

--- offspring_react/src/pages/Home/Home.jsx
+++ offspring_react/src/pages/Home/Home.jsx
@@ -7,9 +7,11 @@ import ausbildung from  "../../assets/ausbildung_card_icon.svg"
 const Home = () => {
   return (
     <div className="container mx-auto p-4 object-center">
-      <h1 className="text-4xl font-bold mb-6 text-center">Offspring</h1>
-      <p className="text-xl mb-6 text-center">Verwalte deine schulischen Leistungen und Berichtshefte bequem an einem Ort, um stets den Überblick zu behalten. Deine Vorgesetzten haben Einsicht in deine Fortschritte und unterstützen dich dabei, stets auf dem richtigen Weg zu bleiben. So profitierst du von einer strukturierten und transparenten Dokumentation, die deinen Fortschritt unterstützt, um Deine Ausbildung bestmöglich zu meistern. </p>
-      <div className="flex flex-wrap justify-center">
+
+<h1 class="mb-4 text-lg font-extrabold text-gray-900 dark:text-white md:text-2xl lg:text-4xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Offspring.</span> Nachwuchs.</h1>
+<p class="font-normal text-gray-500 lg:text-xl dark:text-gray-400">Verwalte deine schulischen Leistungen und Berichtshefte bequem an einem Ort, um stets den Überblick zu behalten. Deine Vorgesetzten haben Einsicht in deine Fortschritte und unterstützen dich dabei, stets auf dem richtigen Weg zu bleiben. So profitierst du von einer strukturierten und transparenten Dokumentation, die deinen Fortschritt unterstützt, um Deine Ausbildung bestmöglich zu meistern. </p>
+
+      <div className="flex flex-wrap justify-center mt-10">
         <FeatureCard title="Berichtshefte" description="Erstellen und Verwalten Deiner wöchentlichen Ausbildungsnachweise" image={berichtshefte} scale={0.70} />
         <FeatureCard title="Noten" description="Schulnoten speichern und Deinen Notendurchschnitt anzeigen" image={noten} scale={0.8} />
         <FeatureCard title="Ausbildung" description="Informationen über Deine schulische und betriebliche Ausbildung" image={ausbildung} scale={0.8}  />
