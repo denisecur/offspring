23f629765676c478f930202e245e310e04f53e8d
denisecur <denise.geiger@icloud.com>
2024-09-30 12:44:46 +0200

    Tabs und Layout verbunden

--- offspring_react/src/components/Dashboard/ChefDashboard.jsx
+++ offspring_react/src/components/Dashboard/ChefDashboard.jsx
@@ -4,11 +4,49 @@ import { SplitScreen } from '../Layout/SplitScreen';
 import AzubiListe from '../AzubiListe';
 import AzubiMonitor from './AzubiMonitor';
 import { fetchAzubis } from '../../api/azubis/azubiService';
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  Box,
+  Typography,
+  Paper,
+} from "@mui/material";
 
 const ChefDashboard = () => {
   const [azubis, setAzubis] = useState([]);
-  const [selectedAzubi, setSelectedAzubi] = useState(null); // Für die Hauptanzeige
+  const [modeTab, setModeTab] = useState('single') // Einzelansicht: single oder Alle-Azubis: multi
+  const [selectedAzubi, setSelectedAzubi] = useState(null); // innerhalb Einzelansicht
 
+
+  const handleModeTabs = (event, newView) => {
+    if(newView !== null) {
+      setModeTab(newView);
+    }
+  };
+
+  const renderModeTabsContent = () => {
+    switch (modeTab) {
+      case 'single':
+        return (
+          <SplitScreen leftWeight={1} rightWeight={3}>
+          <AzubiListe azubis={azubis} onSelectAzubi={setSelectedAzubi} />
+          <AzubiMonitor azubi={selectedAzubi} />
+        </SplitScreen>
+        );
+      case 'multi':
+        return (
+          <SplitScreen leftWeight={1} rightWeight={3}>
+            <Typography variant='body1'>Hier wird die Statsliste sein.</Typography>
+            <Typography variant='body1'>Hier werden dann die zugehörigen Charts angezeigt.</Typography>
+
+        </SplitScreen>
+        );
+        default:
+          return (
+            <p>Es ist kein Tab ausgewählt.</p>
+          );
+    }
+  }
 
   useEffect(() => {
     const loadAzubis = async () => {
@@ -24,10 +62,27 @@ const ChefDashboard = () => {
   }, []);
 
   return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <AzubiListe azubis={azubis} onSelectAzubi={setSelectedAzubi} />
-      <AzubiMonitor azubi={selectedAzubi} />
-    </SplitScreen>
+    <div>
+    <ToggleButtonGroup
+    value={modeTab}
+    exclusive
+    onChange={handleModeTabs}
+    aria-label="modeTab-Ansichtsauswahl"
+    sx={{ ml: "auto" }}
+  >
+    <ToggleButton value="single" aria-label="single">
+      Single
+    </ToggleButton>
+
+    <ToggleButton value="multi" aria-label="multi">
+      Multi
+    </ToggleButton>
+
+  </ToggleButtonGroup>
+
+  {/* Dynamischer Inhalt basierend auf ausgewähltem Tab */}
+  <Box sx={{ mt: 2 }}>{renderModeTabsContent()}</Box>
+  </div>
   );
 };
 
