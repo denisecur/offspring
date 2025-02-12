904746a6f35d1e60802be02da4c54f064928badb
denisecur <denise.geiger@icloud.com>
2025-02-12 13:26:48 +0100

    neue themes definiert

--- offspring_react/tailwind.config.js
+++ offspring_react/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // Standardfarben (können überschrieben werden)
         primary: "#2563eb",
         secondary: "#f472b6",
         accent: "#34d399",
@@ -23,39 +24,39 @@ module.exports = {
       },
       screens: {
         sm: "640px",
-        // => @media (min-width: 640px) { ... }
-
         md: "768px",
-        // => @media (min-width: 768px) { ... }
-
         lg: "1024px",
-        // => @media (min-width: 1024px) { ... }
-
         xl: "1280px",
-        // => @media (min-width: 1280px) { ... }
-
         "2xl": "1536px",
-        // => @media (min-width: 1536px) { ... }
       },
     },
   },
   daisyui: {
     themes: [
       {
-        mytheme: {
-          primary: "#2563eb",
-          secondary: "#f472b6",
-          accent: "#34d399",
-          neutral: "#5eead4",
-          base100: "#ffffff",
+        cp1: {
+          primary: "#ff2a6d",
+          secondary: "#d1f7ff",
+          accent: "#05d9e8",
+          neutral: "#005678",
+          "base-100": "#01012b",
           info: "#fde047",
           success: "#4ade80",
           warning: "#fb923c",
           error: "#ef4444",
-          t1: "#453edb",
-          allgemeinFach: "#8492a6",
-          fachlichFach: "#7e5bef",
         },
+        cp2: {
+          primary: "#7700a6",
+          secondary: "#fe00fe",
+          accent: "#defe47",
+          neutral: "#00b3fe",
+          "base-100": "#0016ee",
+          info: "#fde047",
+          success: "#4ade80",
+          warning: "#fb923c",
+          error: "#ef4444",
+        },
+        // Weitere Themes hier hinzufügen
       },
     ],
   },
